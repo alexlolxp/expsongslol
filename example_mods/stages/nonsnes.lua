0376b4ba68f163ba398aaac5c96a7f5947ce94c1
joemamahahaha <87790979+joemamahahaha@users.noreply.github.com>
2021-10-22 21:12:02 -0400

    gdqdqwd

--- example_mods/stages/nonsnes.lua
+++ example_mods/stages/nonsnes.lua
@@ -1,9 +1,15 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('eteledbg', 'stages/glitchhallway', -600, -300);
-	setLuaSpriteScrollFactor('eteledbg', 0.9, 0.9);
+	makeLuaSprite('outside', 'stages/Outside', -600, 40);
+	setLuaSpriteScrollFactor('outside', 0.8, 0.8);
+	scaleObject('outside', 0.8, 0.8);
 
-	addLuaSprite('eteledbg', false);
+	makeLuaSprite('inside', 'stages/BACKGROUND', -800, -370);
+	setLuaSpriteScrollFactor('inside', 0.9, 0.9);
+	scaleObject('inside', 0.9, 0.9);
+
+	addLuaSprite('outside', false);
+	addLuaSprite('inside', false);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
